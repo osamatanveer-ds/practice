1f8093395c7a957928cdd9ec74500b6d8e790dd7
Osama Tanveer <OsamaTanveer@Developersstudios-MacBook-Air.local>
2021-06-25 18:13:50 +0500

    hhh

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,1 +1,7 @@
 <h2>this is a test file</h2>
+
+@if (isset($_POST['collection_var']))
+                                        Collection Var = {{$_POST['collection_var']}}
+                                    @else
+                                        No Collection variable got '{{'var-'.$_POST['collection_var']}}' empty
+                                    @endif
