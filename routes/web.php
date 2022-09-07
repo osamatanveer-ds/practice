4c4aaccfadc0489104c3c0e825bc0fdbc6cb3af2
Osama Tanveer <osama.tanveer@developers.studio>
2022-09-07 18:06:18 +0500

    auto pull

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/auto', function () {
+    return view('welcome');
+});
 
+Route::get('/auto', function () {
+    return view('welcome');
+});
