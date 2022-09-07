6e3fafbbb92f61b6655563c3b535b6ed46b8cf01
Osama Tanveer <osama.tanveer@developers.studio>
2022-09-07 17:11:56 +0500

    auto pull

--- routes/web.php
+++ routes/web.php
@@ -16,11 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/auto', function () {
-    return view('welcome');
-});
-
-Route::get('/autopull', function () {
-    return view('welcome');
-});
